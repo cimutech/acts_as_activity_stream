e937ad1f6b2925b28b551ecbc2a3b3a96fa1e749
chenqh <chenqh@cimutech.com>
2014-04-02 14:44:43 +0800

    fix activity to_builder, miss activity verb

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -31,12 +31,13 @@ class Activity < ActiveRecord::Base
     Jbuilder.new do |json|
       json.id             id
       json.type           activable_type
+      json.verb           verb
+      json.data           data
+      json.author         author.to_builder.attributes!
       json.likes_count    likes_count
       json.comments_count comments_count
       json.created_at     created_at
       json.updated_at     updated_at
-      json.author         author.to_builder.attributes!
-      json.data           data
     end
   end
 
