b2a9865bcd682cc793d6ade7e798b88ae60f8562
huangll <huangll@cimu.com.cn>
2014-10-11 17:59:42 +0800

    add reply comment function

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,8 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :activity, foreign_key: :commentable_id, class_name: "Activity", counter_cache: true
   belongs_to :parent, class_name: "Comment"
-  has_many   :children, class_name: "Comment"
+  has_many :children, class_name: "Comment", as: :commentable, dependent: :destroy
+
 
   validates_presence_of :body
 
@@ -15,10 +16,23 @@ class Comment < ActiveRecord::Base
       json.id             id
       json.body           body
       json.sender         sender.to_builder.attributes!
-      if parent
-        json.parent_sender  parent.sender.to_builder.attributes!
+      json.sender_time    created_at
+      # if parent
+      #   json.parent_sender  parent.sender.to_builder.attributes!
+      # end
+      if to_activity?
+        json.comments       flat_children.order('created_at').collect { |a| a.to_builder.attributes! }
       end
     end
+  end
+
+  def flat_children
+    children.each do |a|
+      a.flat_children.each do |b|
+        children.build b.attributes
+      end
+    end
+    children
   end
 
   private
@@ -27,4 +41,15 @@ class Comment < ActiveRecord::Base
     title ||= body.truncate(60, :separator =>' ')
   end
 
+  def has_children?
+    !children.blank?
+  end
+
+  def has_parent?
+    !parent.nil?
+  end
+
+  def to_activity?
+    commentable.class.name == 'Activity'
+  end
 end
--- lib/acts_as_activity_stream/actorable.rb
+++ lib/acts_as_activity_stream/actorable.rb
@@ -131,10 +131,15 @@ module ActsAsActivityStream
           activity.likes.create!(sender_id: actor.id)
         end
 
-        def comment(activity, body, parent_id =  nil)
+        def reply_activity(activity, body, parent_id =  nil)
           activity = Activity.find(activity) if activity.is_a?(Integer)
           return nil unless activity.can_read_by?(actor)
           activity.comments.create!(sender_id: actor.id, body: body, parent_id: parent_id)
+        end
+
+        def reply_comment(comment, body)
+          comment = Comment.find(comment) if comment.is_a?(Integer)
+          comment.children.create!(body: body, sender_id: actor.id, parent_id: comment.id)
         end
 
         def wall(type, options = {})
